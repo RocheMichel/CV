da9f6061e0996d6819ea49ab1547627093865165
RocheMichel <135127090+RocheMichel@users.noreply.github.com>
2023-06-22 13:59:33 +0200

    Update header.php

---
+++ header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="CV.css">
+    <meta name="desciption" content="<?=$meta_Description?>" >
+    <title><?=$meta_Titlle ?></title>
+</head>
+
+<body>
+<header>
+    <div id="tete">
+        <div class="content_nom_menu">
+            <div class="nom">
+                <h1>Michel Roche<br>Développeur Web</h1>
+            </div>
+            <nav class="menu">
+
+                <a href="?page=cv">C.V</a><a href="?page=hobbie">Hobbie</a><a href="?page=contact">Contact</a>
+            </nav>
+        </div>
+        <div class="photo"><img src="20230619_174411.jpg" alt="photo de profil" height="200">
+        </div>
+
+    </div><!--Ici fin de la partie en-tête-->
+</header>
