c9891b7a72d85807151261a39a414e48e845d910
Roche <michel.roche@le-campus-numerique.fr>
2023-06-22 13:35:15 +0200

    footer.php

---
+++ cv.php
@@ -0,0 +1,91 @@
+<?php
+$meta_Titlle = 'Contact';
+$meta_Description = 'Formulaire dinformation';
+include 'header.php';
+?>
+
+    <main>
+        <div id="conteneur_main1_main2">
+            <div id="main1">
+                <div id="profil">
+                    <h2>Description</h2>
+                    <ul>
+                        <li>Rigueur</li>
+                        <li>Organisation</li>
+                        <li>Ecoute active</li>
+                        <li>Sens du relationnel</li>
+                        <li>Esprit d'équipe</li>
+                        <li>Gestion du stress</li>
+                        <li>Curiosité</li>
+                        <li>Sens de la logique et de la déduction</li>
+                        <li>Tempérant</li>
+                    </ul>
+                </div><!--Fin de la partie description-->
+                <div id="compétence">
+                    <h2>Compétences</h2>
+                    <h3>Travaux publique</h3>
+                    <ul>
+                        <li>Préparation des outils</li>
+                        <li>Pause de bordure, regard, canalisation et filé de sécurité</li>
+                        <li>Préparation de la chaussée en 0.30, pour imprégnation be/couche ou enrobé </li>
+                        <li>Conduite d'engin</li>
+                        <li>Mettrage et implantation</li>
+                    </ul>
+                    <h3>Vente en magasin</h3>
+                    <ul>
+                        <li>Acceuil des clients</li>
+                        <li>Vente conseil</li>
+                        <li>Implantation de rayon</li>
+                        <li>Reception de la marchandise</li>
+                        <li>Mise en rayon</li>
+                        <li>Nettoyage de la surface de vente</li>
+                    </ul>
+                    <h3>Technico-commercial</h3>
+                    <ul>
+                        <li>Préparation de la zone de prospection</li>
+                        <li>Démarchage porte à porte</li>
+                        <li>Analyser la demande et la compatibilité du projet du client celon les contraintes</li>
+                        <li>Mettrage</li>
+                        <li>Etablir des devis</li>
+                        <li>Suivis chantier</li>
+                        <li>Phoning</li>
+                        <li>Relance client</li>
+                    </ul>
+                    <h3>Ouvrier de production</h3>
+                    <ul>
+                        <li>Approvisionnement de machine de production</li>
+                        <li>Vérification du déroulement de la chaine de production et de sa qualité</li>
+                        <li>Conditionnement</li>
+                        <li>Manutation divers avec le transpalette électrique</li>
+                    </ul>
+                    <h3>Soudeur</h3>
+                    <ul>
+                        <li>Organisation du poste de travail</li>
+                        <li>Mise en place des différents profilé</li>
+                        <li>Lecture de plan</li>
+                        <li>Soudage en positions (M.I.G)</li>
+                        <li>Réparation et control des défauts de soudure</li>
+                        <li>Manutention au pont de levage</li>
+                        <li>Former les intérimaires</li>
+                        <li>Perçage, taraudage, découpe plasma</li>
+                        <li>Gérer la partie production</li>
+                        <li>Assuré les réparations des défauts de production ou apporter des modifications</li>
+                    </ul>
+                    <h3>Préparatoire aux métier du numérique</h3>
+                    <ul>
+                        <li>Algorythmie</li>
+                        <li>Exercice sur le site France IOI découverte du langage Python (établir des bases de calcul,
+                        faire effectuer des déplacement à un robot)</li>
+                        <li>Renforcement des savoirs en math, Français, logique</li>
+                        <li>Cours d'anglais</li>
+                        <li>Etablir la tructure du page Html, élaboration de liste, tableau, lien, video, image, son
+                        </li>
+                    </ul><!--Fin de la partie compétences-->
+                </div>
+            </div>
+        </div>
+    </main>
+
+<?php
+include 'Footer.php';
+?>
---
+++ hobbie.php
@@ -0,0 +1,52 @@
+<?php
+$meta_Titlle = 'Hobbie';
+$meta_Description = 'Mes Hobbies';
+include 'header.php';
+?>
+
+<h1>Mes Hobbie</h1>
+<ul>
+    <li>Dessin</li>
+    <li>Musculation</li>
+    <li>Cinéma</li>
+    <li>Jeux-vidéo</li>
+    <li>Méditation et développemnt personnel</li>
+</ul>
+<div class="video_hobbie">
+    <h2>Le Dessin</h2>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/U6U2nz-8Xp4" title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowfullscreen></iframe>
+</div>
+<div>
+    <h2>Musculation</h2>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/Yk4kA-q_IVU" title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowfullscreen></iframe>
+</div>
+<div>
+    <h2>Cinéma</h2>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/KLz_tKXHXgw" title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowfullscreen></iframe>
+</div>
+<div>
+    <h2>Jeux-video</h2>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/wrn9LJWmm3k" title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowfullscreen></iframe>
+</div>
+<div>
+    <h2>Méditation et développement personnel</h2>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/bXKKhOadDu8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+</div>
+
+<?php
+include 'Footer.php';
+?>
+
+
